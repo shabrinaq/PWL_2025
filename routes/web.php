c16b635d73df0ed2a89586f6ac8bb09a41d68e6b
shabrinaq <shabrinaqottrunnada@gmail.com>
2025-02-20 13:23:18 +0700

    Modify Practicum 1 no. g

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return 'welcome';
+Route::get('/about', function () {
+    return '2341760160 Shabrina Qottrunnada';
 });
