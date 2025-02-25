4911674678fb3b01f4c3c58a0dc1c4e18a227c9e
shabrinaq <shabrinaqottrunnada@gmail.com>
2025-02-25 21:43:18 +0700

    Step View

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
-]);
-
-Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
-]);
-
+Route::get('/greeting', [WelcomeController::class, 'greeting']);   
+    
 
